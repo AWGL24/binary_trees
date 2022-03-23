e83fe21648f52e8f18d670768570af872b5c779a
Drew-24 <andrewwil1@hotmail.com>
2022-03-23 17:34:44 -0400

    Fixed code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || (tree->left && tree->right == NULL))
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	left = binary_tree_height(tree->left);
